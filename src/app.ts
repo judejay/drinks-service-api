5fc963f5a4c4972c1c969010b66a489342c8921a
judejay <judejay26@gmail.com>
2023-11-22 12:54:35 +0000

    feat: Tea controller with route

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,5 @@ app.use(express.json());
 
 app.get('/', (req, res) => res.send('Welcome to the Drinks API!'));
 
-//app.get('/coffeelover', (req, res) => res.send('I like coffee!'));
 
 app.use('/', router);

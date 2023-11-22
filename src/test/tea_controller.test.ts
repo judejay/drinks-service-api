005b190e4b3aaf89d53652c3ee4d507a708366af
judejay <judejay26@gmail.com>
2023-11-22 12:57:32 +0000

    feat: Tea endpoint returns query  in object

--- src/test/tea_controller.test.ts
+++ src/test/tea_controller.test.ts
@@ -9,3 +9,15 @@ drinkType: 'Tea',
 name: 'Ceylon',
 });
 });
+
+
+test('GET /tea should return correct object with different drink', async () => {
+    const res = await request(app)
+    .get('/tea') 
+    .query({ teaName: 'Darjeeling' });   
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({
+    drinkType: 'Tea',
+    name: 'Darjeeling',
+    });
+    });
